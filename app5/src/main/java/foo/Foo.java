7f2eac6222762eb06ae6ba61b403bdb43b2cd997
githubfoam <lequestionis@gmail.com>
2020-11-15 16:57:41 +0300

    foo fix

--- app5/src/main/java/foo/Foo.java
+++ app5/src/main/java/foo/Foo.java
@@ -7,9 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Foo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Foo.class, args);
 	}
 }
